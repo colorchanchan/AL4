8062cba148f18d63dfafd33b9e649d9404dfbc46
colorchanchan <115062603+colorchanchan@users.noreply.github.com>
2023-10-04 13:01:47 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Player.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include <memory>
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	// ビュープロジェクション(共通)
+	ViewProjection viewProjection_;
+	// モデル
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// 3Dモデル
+	std::unique_ptr<Model> model_;
+	std::unique_ptr<Player> player_;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+};
